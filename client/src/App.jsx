4e9bc8b001bcc07dabbc1da8c436b87289b79bba
GaganUppaldev <ugaganajitsinghuppal@gmail.com>
2025-02-03 23:55:25 +0530

    Woeking on react to make pieces move from one location to other on clicking the cells in board

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,6 @@
+//import react from "react";
+import {useState} from "react"; //
+
 function App(){
   function arr(){
     const board = [] //2d array
@@ -34,20 +37,70 @@ function App(){
 
     return board
   }
+
+  
+
+  const[board,setBoard]= useState(arr());
+  const[selected,setSelected]=useState(null);
+
+  
+
+    
+
+    //click
+    function handleClick(row,col){
+      if(selected){
+        move(row,col);
+
+      }else{
+        setSelected({row,col});//{}
+
+      }
+
+    }
+
+    function move(newRow , newCol){
+      if(!selected) return;
+
+      const newboard = [...board];//copied board
+      newboard[newRow][newCol].piece = newboard[selected.row][selected.col].piece;
+      newboard[selected.row][selected.col].piece = null;
+
+      setBoard(newboard);
+      setSelected(null);
+
+
+    }
+
+
+
+
+
+  
   const print = arr();
   console.log(print);
   return(
     <>
     <h1>Chess</h1>
-    {print.map((row,rowindex) => (
-      <div key={rowindex} style={{display: "flex"}}>
-        {row.map((cell,celindex) => (
-          <span key={`${rowindex}-${celindex}`}
+    {print.map((row,rowIndex) => (
+      <div key={rowIndex} style={{display: "flex"}}>
+        {row.map((cell,colIndex) => (
+          <span key={`${rowIndex}-${colIndex}`}
+          onClick={() => handleClick(rowIndex, colIndex)}
           style = {{
             width : "70px",
             height : "50px",
             backgroundColor: cell.color === "W" ? "white" : "gray",
             border: "1px solid red",
+            border:
+                  selected?.row === rowIndex && selected?.col === colIndex
+                    ? "2px solid pink" // 
+                    : "1px solid red",
+                fontSize: "20px",
+                fontWeight: "bold",
+                cursor: "pointer",
+
+            
           }} 
 
           
