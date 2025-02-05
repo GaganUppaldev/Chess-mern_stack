ec2d67214d7bcb2f152bdbe6056ca6ca056d05a7
GaganUppaldev <ugaganajitsinghuppal@gmail.com>
2025-02-05 23:45:03 +0530

    using usestate now movements are possible in chess board

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,129 +1,80 @@
-//import react from "react";
-import {useState} from "react"; //
+import { useState } from "react";
 
-function App(){
-  function arr(){
-    const board = [] //2d array
-    for(let i = 0 ; i < 8; i++){
-      board[i] = []
-      for(let j =0 ; j < 8 ;j++){
-        //board[i][j] = "0" ;
+function App() {
+  function arr() {
+    const board = []; // 2D array
+    for (let i = 0; i < 8; i++) {
+      board[i] = [];
+      for (let j = 0; j < 8; j++) {
         board[i][j] = {
-          color : (i+j) %2 === 0 ? "W" : "B",
-          piece : null ,
-          
-        }
-
-
+          color: (i + j) % 2 === 0 ? "W" : "B",
+          piece: null,
+        };
       }
-
     }
 
-    for(let j = 0 ; j<8 ; j++){
-      board[1][j].piece = "pawn" ;
-      board[6][j].piece = "pawn" ;
+    // Place pawns
+    for (let j = 0; j < 8; j++) {
+      board[1][j].piece = "pawn";
+      board[6][j].piece = "pawn";
     }
 
-    for(let j = 0 ; j <8 ;j++){
-      const stuff = ["elephant","horse","Minster" ,"King","Queen","elephant","horse","Minster"]
+    // Place other pieces
+    const stuff = ["elephant", "horse", "minister", "King", "Queen", "elephant", "horse", "minister"];
+    for (let j = 0; j < 8; j++) {
       board[0][j].piece = stuff[j];
       board[7][j].piece = stuff[j];
     }
 
-    
-    
-
-    
-
-    return board
+    return board;
   }
 
-  
+  const [board, setBoard] = useState(arr());
+  const [selectedPiece, setSelectedPiece] = useState(null);
 
-  const[board,setBoard]= useState(arr());
-  const[selected,setSelected]=useState(null);
+  const handleClick = (row, col) => {
+    // Copy the board state to avoid direct mutation
+    const newBoard = [...board];
 
-  
-
-    
-
-    //click
-    function handleClick(row,col){
-      if(selected){
-        move(row,col);
-
-      }else{
-        setSelected({row,col});//{}
-
-      }
-
+    if (!selectedPiece && newBoard[row][col].piece) {
+      // Select the piece
+      setSelectedPiece({ row, col, piece: newBoard[row][col].piece });
+    } else if (selectedPiece) {
+      // Move the piece to the new cell
+      newBoard[selectedPiece.row][selectedPiece.col].piece = null;
+      newBoard[row][col].piece = selectedPiece.piece;
+      setSelectedPiece(null);
+      setBoard(newBoard);
     }
+  };
 
-    function move(newRow , newCol){
-      if(!selected) return;
-
-      const newboard = [...board];//copied board
-      newboard[newRow][newCol].piece = newboard[selected.row][selected.col].piece;
-      newboard[selected.row][selected.col].piece = null;
-
-      setBoard(newboard);
-      setSelected(null);
-
-
-    }
-
-
-
-
-
-  
-  const print = arr();
-  console.log(print);
-  return(
+  return (
     <>
-    <h1>Chess</h1>
-    {print.map((row,rowIndex) => (
-      <div key={rowIndex} style={{display: "flex"}}>
-        {row.map((cell,colIndex) => (
-          <span key={`${rowIndex}-${colIndex}`}
-          onClick={() => handleClick(rowIndex, colIndex)}
-          style = {{
-            width : "70px",
-            height : "50px",
-            backgroundColor: cell.color === "W" ? "white" : "gray",
-            border: "1px solid red",
-            border:
-                  selected?.row === rowIndex && selected?.col === colIndex
-                    ? "2px solid pink" // 
-                    : "1px solid red",
-                fontSize: "20px",
-                fontWeight: "bold",
+      <h1>Chess</h1>
+      {board.map((row, rowIndex) => (
+        <div key={rowIndex} style={{ display: "flex" }}>
+          {row.map((cell, cellIndex) => (
+            <span
+              key={`${rowIndex}-${cellIndex}`}
+              onClick={() => handleClick(rowIndex, cellIndex)}
+              style={{
+                width: "70px",
+                height: "50px",
+                backgroundColor: cell.color === "W" ? "white" : "gray",
+                border: "1px solid red",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
                 cursor: "pointer",
-
-            
-          }} 
-
-          
-          >
-            
-            {cell.piece || ""}
-
-
-          </span>
-          
-
-        
-        ))}
-
-      </div>
-    ))}
-    
+              }}
+            >
+              {cell.piece || ""}
+            </span>
+          ))}
+        </div>
+      ))}
     </>
-    
-    
-  
-  )
+  );
 }
 
 export default App;
-
